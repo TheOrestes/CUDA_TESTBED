f7333898dcb591629da4fedbe16a44b207636184
TheOrestes <rupeshmandke@outlook.com>
2025-12-13 23:54:22 +0530

    Added Simple matrix multiplication kernel

--- Kernel.cu
+++ Kernel.cu
@@ -1,6 +1,8 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
+#include <device_launch_parameters.h>
 
 #include "cuda_runtime.h"
 
@@ -72,11 +74,84 @@ void VectorAddition()
 }
 
 //---------------------------------------------------------------------------------------------------------------------
+__global__ void MatrixMult(const float* a, const float* b, float* ans, int N)
+{
+	// calculate unique thread index
+	int col = blockDim.x * blockIdx.x + threadIdx.x;
+	int row = blockDim.y * blockIdx.y + threadIdx.y;
+
+	// boundary check
+	if(row < N && col < N)
+	{
+		float value = 0.0f;
+
+		for(int i = 0 ; i < N ; i++)
+		{
+			value += a[row * N + i] * b[i * N + col];
+		}
+
+		ans[row * N + col] = value;
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void MatrixMultiplication()
+{
+	int N = 512;
+	size_t SIZE = N * N * sizeof(float);
+
+	float* cpuMatrixInput1 = static_cast<float*>(malloc(SIZE));
+	float* cpuMatrixInput2 = static_cast<float*>(malloc(SIZE));
+	float* cpuMatrixResult = static_cast<float*>(malloc(SIZE));
+
+	// Initialize host array
+	for (int i = 0; i < N * N; ++i)
+	{
+		cpuMatrixInput1[i] = sin(i);
+		cpuMatrixInput2[i] = cos(i);
+	}
+
+	// Allocate Memory on the Device/GPU
+	float* gpuMatrix1; float* gpuMatrix2; float* gpuResult;
+	cudaMalloc((void**)&gpuMatrix1, SIZE);
+	cudaMalloc((void**)&gpuMatrix2, SIZE);
+	cudaMalloc((void**)&gpuResult, SIZE);
+
+	// Copy data from CPU to GPU
+	cudaMemcpy(gpuMatrix1, cpuMatrixInput1, SIZE, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuMatrix2, cpuMatrixInput2, SIZE, cudaMemcpyHostToDevice);
+
+	// configure launch kernel parameter
+	dim3 threadPerBLock(16, 16);
+	dim3 blocksPerGrid((N + threadPerBLock.x - 1) / threadPerBLock.x, 
+						(N + threadPerBLock.y - 1) / threadPerBLock.y);
+	// Launch Kernel
+	MatrixMult << <blocksPerGrid, threadPerBLock >> > (gpuMatrix1, gpuMatrix2, gpuResult, N);
+
+	// Copy result back from the GPU to CPU!
+	cudaMemcpy(cpuMatrixResult, gpuResult, SIZE, cudaMemcpyDeviceToHost);
+
+	// Print Result
+	for (int i = 0; i < N; ++i)
+	{
+		printf("%f ", cpuMatrixResult[i]);
+	}
+
+	cudaFree(gpuMatrix1);
+	cudaFree(gpuMatrix2);
+	cudaFree(gpuResult);
+	free(cpuMatrixInput1);
+	free(cpuMatrixInput2);
+	free(cpuMatrixResult);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
 int main()
 {
 	printf("\n--------------- Start from the CPU!\n");
 
-	VectorAddition();
+	//VectorAddition();
+	MatrixMultiplication();
 
 	printf("\n--------------- End from the CPU!\n");
 
